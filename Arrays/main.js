979ed75b244278b6a7b9b3e5c1f865dbe2f7e647
Tygo <40080@ma-web.nl>
2026-01-26 09:34:35 +0100

    les-8-2

--- Arrays/main.js
+++ Arrays/main.js
@@ -1,102 +1,76 @@
-    // Start array (minimaal 5 items, 3+ datatypes)
+// array met verschillende dingen
 let arr = [10, "appel", true, 5.5, { naam: "Pikachu" }];
-console.log("Start:", arr);
+console.log(arr);
 
 // length
-console.log("length:", arr.length);
+console.log(arr.length);
+
+// toString
+console.log(arr.toString());
+
+// at
+console.log(arr.at(1));
+
+// join
+console.log(arr.join(" | "));
+
+// pop
+arr.pop();
 console.log(arr);
 
-// toString()
-let r_toString = arr.toString();
-console.log("toString:", r_toString);
+// push
+arr.push("nieuw");
 console.log(arr);
 
-// at()
-let r_at = arr.at(1);
-console.log("at(1):", r_at);
+// shift
+arr.shift();
 console.log(arr);
 
-// join()
-let r_join = arr.join(" | ");
-console.log("join:", r_join);
-console.log(arr);
-
-// pop()
-let r_pop = arr.pop();
-console.log("pop:", r_pop);
-console.log(arr);
-
-// push()
-let r_push = arr.push("nieuw");
-console.log("push:", r_push);
-console.log(arr);
-
-// shift()
-let r_shift = arr.shift();
-console.log("shift:", r_shift);
-console.log(arr);
-
-// unshift()
-let r_unshift = arr.unshift(99);
-console.log("unshift:", r_unshift);
+// unshift
+arr.unshift(99);
 console.log(arr);
 
 // delete
 delete arr[1];
-console.log("delete index 1");
 console.log(arr);
 
-// concat()
+// concat
 let extra = ["extra", false];
-let r_concat = arr.concat(extra);
-console.log("concat:", r_concat);
+console.log(arr.concat(extra));
+
+// copyWithin
+arr.copyWithin(0, 1);
 console.log(arr);
 
-// copyWithin()
-let r_copyWithin = arr.copyWithin(0, 1);
-console.log("copyWithin:", r_copyWithin);
-console.log(arr);
-
-// flat()
+// flat
 let nested = [1, [2, [3, 4]]];
-let r_flat = nested.flat(2);
-console.log("flat:", r_flat);
+console.log(nested.flat(2));
 
-// splice()
-let r_splice = arr.splice(1, 1, "vervangen");
-console.log("splice removed:", r_splice);
+// splice
+arr.splice(1, 1, "vervangen");
 console.log(arr);
 
-// toSpliced()
-let r_toSpliced = arr.toSpliced(1, 1, "nieuwItem");
-console.log("toSpliced:", r_toSpliced);
-console.log(arr);
+// toSpliced
+let nieuw = arr.toSpliced(1, 1, "nieuwItem");
+console.log(nieuw);
 
-// slice()
-let r_slice = arr.slice(1, 3);
-console.log("slice:", r_slice);
-console.log(arr);
+// slice
+console.log(arr.slice(1, 3));
 
-// indexOf()
-let r_indexOf = arr.indexOf("nieuw");
-console.log("indexOf:", r_indexOf);
-console.log(arr);
+// indexOf
+console.log(arr.indexOf("nieuw"));
 
-// lastIndexOf()
-let r_lastIndexOf = arr.lastIndexOf("nieuw");
-console.log("lastIndexOf:", r_lastIndexOf);
-console.log(arr);
+// lastIndexOf
+console.log(arr.lastIndexOf("nieuw"));
 
-// includes()
-let r_includes = arr.includes(true);
-console.log("includes true:", r_includes);
-console.log(arr);
+// includes
+console.log(arr.includes(true));
 
-// sort()
-let sortArray = [3, 1, 5, 2];
-sortArray.sort();
-console.log("sort:", sortArray);
+// sort
+let nums = [3, 1, 5, 2];
+nums.sort();
+console.log(nums);
 
-// reverse()
-sortArray.reverse();
-console.log("reverse:", sortArray);
+// reverse
+nums.reverse();
+console.log(nums);
